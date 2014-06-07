8c1d68eda5d31056e2090db697cd0b7e181bead1
Miguel Eduardo Roman Martinez <miguelerm@gmail.com>
2014-06-07 08:35:38 -0600

    Se agrego el servicio rest personas.

---
+++ BaseDeDatos/Controllers/PersonasController.cs
@@ -0,0 +1,91 @@
+﻿using BaseDeDatos.Models;
+using BaseDeDatos.Servicios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BaseDeDatos.Controllers
+{
+    public class PersonasController : ApiController
+    {
+
+        private readonly PersonasServicio servicio;
+
+        public PersonasController()
+        {
+            this.servicio = new PersonasServicio();
+        }
+
+        public IHttpActionResult Get(string sortBy = null, string criterio = null)
+        {
+            IEnumerable<PersonaModelo> result;
+
+            if (!string.IsNullOrWhiteSpace(criterio))
+            {
+                result = servicio.ObtenerPorBusqueda(criterio);
+            }
+            else
+            {
+                result = servicio.ObtenerTodas(sortBy);
+            }
+
+            return Ok(result);            
+        }
+
+        public IHttpActionResult Get(string id)
+        {
+            var result = servicio.ObtenerUnica(id);
+
+            if (result == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
+        }
+
+        public IHttpActionResult Post(PersonaModelo modelo)
+        {
+            if (modelo == null)
+            {
+                return BadRequest("Debe especificar un modelo");
+            }
+
+            servicio.Crear(modelo);
+            return Created(Url.Link("DefaultApi", new { id = modelo.NifPersona }), modelo);
+        }
+
+        public IHttpActionResult Put(string id, PersonaModelo modelo)
+        {
+            if (modelo == null)
+            {
+                return BadRequest("Debe especificar un modelo");
+            }
+
+            if (!servicio.ExisteNifPersona(id))
+            {
+                return NotFound();
+            }
+
+            modelo.NifPersona = id;
+            servicio.Editar(modelo);
+
+            return this.Ok(modelo);
+        }
+
+        public IHttpActionResult Delete(string id)
+        {
+
+            if (!servicio.ExisteNifPersona(id))
+            {
+                return NotFound();
+            }
+
+            servicio.Eliminar(id);
+            return Ok();
+        }
+    }
+}
--- BaseDeDatos/Global.asax.cs
+++ BaseDeDatos/Global.asax.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Optimization;
 using System.Web.Routing;
 using System.Web.Security;
@@ -16,6 +17,16 @@ namespace BaseDeDatos
             // Code that runs on application startup
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            GlobalConfiguration.Configure(config => {
+                config.MapHttpAttributeRoutes();
+
+                config.Routes.MapHttpRoute(
+                    name: "DefaultApi",
+                    routeTemplate: "api/{controller}/{id}",
+                    defaults: new { id = RouteParameter.Optional }
+                );
+            });
         }
     }
 }
